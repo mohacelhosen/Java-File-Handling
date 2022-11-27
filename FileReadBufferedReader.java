6a1021e35dc72095919056db4c7ab2b6246bc638
Mohacel <118649370+mohacelhosen@users.noreply.github.com>
2022-11-27 11:43:03 +0600

    Add files via upload

---
+++ FileReadBufferedReader.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class FileReadBufferedReader {
+    public static void main(String[] args) throws Exception{
+        FileReader fr = new FileReader("myBirdInfo.txt");
+        BufferedReader br = new BufferedReader(fr);
+        String line = br.readLine(); //read a line of text and return as string
+        while (line !=null){
+            System.out.println(line);
+            line = br.readLine();
+        }
+    }
+}
